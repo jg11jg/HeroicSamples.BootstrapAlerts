046b296aad3bdcdbe9f5fa45bc3fb87b6fe9cf9f
John Gibbons <jg11jg@outlook.com>
2018-08-28 20:30:01 +0100

    Added RazorPAges example

---
+++ HeroicSamples.BootstrapAlerts/Extensions/PageModelExtensions.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HeroicSamples.BootstrapAlerts.Extensions
+{
+    public static class PageModelExtensions
+    {
+        public static PageModel WithSuccess(this PageModel pageModel, string title, string body)
+        {
+            return pageModel.WithAlert("success", title, body);
+        }
+
+        public static PageModel WithWarning(this PageModel pageModel, string title, string body)
+        {
+            return pageModel.WithAlert("warning", title, body);
+        }
+
+        public static PageModel WithDanger(this PageModel pageModel, string title, string body)
+        {
+            return pageModel.WithAlert("danger", title, body);
+        }
+
+        private static PageModel WithAlert(this PageModel pageModel, string type, string title, string body)
+        {
+            var tempData = pageModel.TempData;
+
+            tempData["_alert.type"] = type;
+            tempData["_alert.title"] = title;
+            tempData["_alert.body"] = body;
+
+            return pageModel;
+        }
+    }
+}
---
+++ HeroicSamples.BootstrapAlerts/Pages/Razor.cshtml.cs
@@ -0,0 +1,14 @@
+﻿using HeroicSamples.BootstrapAlerts.Extensions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HeroicSamples.BootstrapAlerts.Pages
+{
+    public class RazorModel : PageModel
+    {
+        public IActionResult OnGet()
+        {
+            return this.WithSuccess("From Razor!", "This was done using a razor extension").Page();
+        }
+    }
+}
